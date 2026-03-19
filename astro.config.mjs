906de8855168f36eede21086deacc5da22712491
Jose Joya <ing.jose.joya@gmail.com>
2026-03-19 18:15:37 -0500

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 import vercel from "@astrojs/vercel";
 
-const IS_DEV = process.env.NODE_ENV === "development";
+const isProduction = process.env.VERCEL === "1";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
-  adapter: IS_DEV ? node({ mode: "standalone" }) : vercel(),
+  adapter: isProduction ? vercel() : node({ mode: "standalone" }),
   server: {
     port: 3000,
     host: true,
